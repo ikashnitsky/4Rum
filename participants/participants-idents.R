3758124e93ff8b4cd6ec8823d11c5a029d9ad851
Ilya Kashnitsky <ilya.kashnitsky@gmail.com>
2025-09-22 14:04:29 +0200

    fix typo

--- participants/participants-idents.R
+++ participants/participants-idents.R
@@ -63,7 +63,7 @@ load("participants/dst-all-contacts.rda")
 
 # additional -- people asked to be included via email ---------------------
 
-idents_via_email_requests <- c("RIE", "SLF", "KLE", "AKE", "MNT", "JOC", "PSL", "LHM", "EHE", "SDN", "RJD", "ALU", "ABN", "SKR", "AKA", "JPM", "KWB")
+idents_via_email_requests <- c("RIE", "SLF", "KLE", "AKE", "MNT", "JOC", "PSL", "LHM", "EHE", "SDN", "RJD", "ALU", "ABN", "SKR", "AKA", "GPM", "KWB")
 
 
 # additional -- R course participants -------------------------------------
